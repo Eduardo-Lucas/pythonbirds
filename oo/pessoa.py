c69ff8769384ebf1e340e7ff666b5a3dd8852fc3
Eduardo Lucas <eduardolucas40@gmail.com>
2020-02-29 17:13:27 -0300

    Criado e removido atributo dinâmico de objetos do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,7 +9,7 @@ class Pessoa:
 
 
 if __name__ == '__main__':
-    aline = Pessoa(nome='Aline')
+    aline = Pessoa(nome='Aline', idade=24)
     eduardo = Pessoa(aline, nome='Eduardo')
     print(Pessoa.cumprimentar(eduardo))
     print(id(eduardo))
@@ -20,3 +20,7 @@ if __name__ == '__main__':
     for filho in eduardo.filhos:
         print(f'Filhos: [{filho.nome}, ]')
 
+    eduardo.sobrenome = 'Lucas'
+    del eduardo.sobrenome
+    print(eduardo.__dict__)
+    print(aline.__dict__)
