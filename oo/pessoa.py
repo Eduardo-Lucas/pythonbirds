08c8c3cd08172e387c70c472af23954cb2629e20
Eduardo Lucas <eduardolucas40@gmail.com>
2020-02-29 16:44:53 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,22 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=58):
+    def __init__(self, *filhos, nome=None, idade=58):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Luciano')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Eduardo'
-    print(p.nome)
+    aline = Pessoa(nome='Aline')
+    eduardo = Pessoa(aline, nome='Eduardo')
+    print(Pessoa.cumprimentar(eduardo))
+    print(id(eduardo))
+    print(eduardo.cumprimentar())
+    print(f'Nome: {eduardo.nome}')
+    print(f'Idade: {eduardo.idade}')
+
+    for filho in eduardo.filhos:
+        print(f'Filhos: [{filho.nome}, ]')
 
